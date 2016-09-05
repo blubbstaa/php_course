4ebf2435402d04c09b4ab8831e9b30caf37e0916
BacLuc <lucius.bachmann@gmx.ch>
2016-09-05 22:54:54 +0100

    added array operator in strings

--- 01_First_Lecture/02_string_manipulation.php
+++ 01_First_Lecture/02_string_manipulation.php
@@ -99,3 +99,54 @@ $exerciceSheet->addExercice(new Exercice("
     if you made it right, the text below should be
     Your \$variable1 is 'yet another string this string is appended'
     ",$testFunction));
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+
+/*
+ * BEGIN DESCRIPTION
+Strings in (and any other programming language) are really just lists of characters
+Thats why we can use the array operator
+with the array operator, we can get a certain character of a string:
+let's say we have a string'This is a string', like this:
+To get the first character of this string, we just use
+$firstcharacter = 'This is a string'[0]
+
+As you noticed, the first character is not character 1, but character 0.
+In programming, we begin with 0.
+ * END DESCRIPTION
+ */
+
+
+$testFunction = function(){
+    /*BEGIN TODO
+    create a variable $variable1 and assign the third letter of the string 'This is a string' to it
+    */
+
+    //END TODO
+
+
+    if(!isset($variable1)){
+        $variable1 = null;
+    }
+
+
+
+    print("<br>Your \$variable1 is '$variable1'.<br>");
+    print("<br>The third character of  This is a string' is 'i'.<br>");
+
+    if($variable1 === 'i' ){
+        return true;
+    }
+
+    return false;
+};
+
+$exerciceSheet->addExercice(new Exercice("
+
+       <h4>Array operator in Strings</h4>
+    if you made it right, the text below should be
+    Your \$variable1 is 'yet another string this string is appended'
+    ",$testFunction));
