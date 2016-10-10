0804a99c899a4d2c51f8ed0bb20ae86234e76158
BacLuc <lucius.bachmann@gmx.ch>
2016-10-10 11:30:00 +0100

    added additional student

--- 01_First_Lecture/12_arrays_and_loops.php
+++ 01_First_Lecture/12_arrays_and_loops.php
@@ -250,6 +250,117 @@ $testFunction = function(){
 
     print("<br>Your \$students is  now:<br>");
     print_r($students);
+    $shouldbe = 'exercise book';
+    if(count($students)==0){
+        return false;
+    }
+    $studentsContent = null;
+
+    foreach($students as $key => $value){
+        if($studentsContent == null){
+            $studentsContent = $value;
+        }
+        if($studentsContent != $value){
+            print("<br>You have made an error, not all students have the same<br>");
+            return false;
+        }
+    }
+
+
+    if($studentsContent== $shouldbe && $students['Franz']==$shouldbe){
+        return true;
+    }elseif( $students['Franz']!=$shouldbe){
+        print("<br>Error: you forgot Franz<br>");
+    }
+    print("<br>You have made an error, all students should have 'textbook'<br>");
+    return false;
+};
+
+$exerciceSheet->addExercice(new Exercice("
+    
+       <h4>Textbook with loop and additional student</h4>
+    if you made it right, you should not get error message. 
+    ",$testFunction));
+
+
+
+$testFunction = function(){
+
+
+    /*BEGIN DESCRIPTION
+        Now, we can also use this for calculation.
+        Lets say, not every
+    */
+    print('Result of Code in Description<br>');
+    $students = array();
+    $students['Albin']=""; //we just prepare, we don't know yet,
+    //what we have to do with them. So we put an empty string inside
+    $students['Joshua']="";
+    $students['Clara']="";
+    $students['Aisha']="";
+    $students['Marck']="";
+
+    //as always when we print our variables after we did a task.
+//with an array, you have to use print_r instead of print.
+//to print an array, you always to like this:
+    print('Initialisation of Array<br>');
+    print_r($students);
+    print('<br>');
+    //Now, we have to give everyone a pencil.
+    //so normally, we would to:
+    $toGive = 'pencil'; //if we have to give another thing later, we can use the same code, just change this variable
+
+    foreach ($students as $studentname => $value){
+        print('--------------------------------------------------------------------------<br><br>');
+        print('Start of foreach loop, which is executed for each element in $students<br>');
+        print('$students is at start of loop iteration<br>');
+        print_r($students);
+        print('<br>');
+        print('Before the code is executed, the key is copied in the variable after the as<br>');
+        print('Which is in this case in this iteration: $studentname : \''.$studentname.'\' <br>');
+        print('and the value is copied in the variable after the =><br>');
+        print('Which is in this case in this iteration: $value : \''.$value.'\' <br>');
+
+
+        //now, we give the student with the current $studentname what we have to give:
+        print('<br><br>Code which is executed in this loop:<br>');
+        print('$students[$studentname]=$toGive;<br>');
+        print('And because $studentname is currently \''.$studentname.'\', this is the same as<br>');
+        print('$students[\''.$studentname.'\']=$toGive;<br>');
+        $students[$studentname]=$toGive;
+        print('<br><br>');
+        print('$students is at end of loop iteration<br>');
+        print_r($students);
+        print('<br>');
+
+        print('--------------------------------------------------------------------------<br><br>');
+    }
+
+    print('After giving everyone pencil<br>');
+    print_r($students);
+    print('<br>');
+
+    print('End Result of Code in Description<br>');
+    unset($students);
+    unset($toGive);
+
+
+    //END DESCRIPTION
+
+
+    /*
+     BEGIN TODO:
+    Now, do the same, but this time give everyone 'textbook'
+     */
+
+//END TODO
+    if(!isset($students)){
+        $students = array();
+    }
+
+
+    print("<br>Your \$students is  now:<br>");
+    print_r($students);
 
     if(count($students)==0){
         return false;
