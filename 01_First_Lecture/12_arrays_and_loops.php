9c7d57f70b720af9fb3defc49d48d748e63e233a
BacLuc <lucius.bachmann@gmx.ch>
2016-10-10 11:26:26 +0100

    added loop

--- 01_First_Lecture/12_arrays_and_loops.php
+++ 01_First_Lecture/12_arrays_and_loops.php
@@ -111,7 +111,79 @@ $exerciceSheet->addExercice(new Exercice("
 $testFunction = function(){
 
 
+    /*BEGIN DESCRIPTION
+        As you see, we write for every student more or less the same:
+        $students['Albin']=$toGive;
+        $students['Joshua']=$toGive;
+        The only thing that changes is the key, the name of the student.
 
+        Wouldn't it be nice if you could say:
+        Do that for every student:
+            give him $toGive ?
+
+        We have this possibility with the foreach loop.
+        foreach($array as $key => $value){
+            //do something
+        }
+        This means: for each element in $array, put the key in $key, put the value in $value, and then execute the script.
+
+        Here an example
+    */
+    print('Result of Code in Description<br>');
+    $students = array();
+    $students['Albin']=""; //we just prepare, we don't know yet,
+    //what we have to do with them. So we put an empty string inside
+    $students['Joshua']="";
+    $students['Clara']="";
+    $students['Aisha']="";
+    $students['Marck']="";
+
+    //as always when we print our variables after we did a task.
+//with an array, you have to use print_r instead of print.
+//to print an array, you always to like this:
+    print('Initialisation of Array<br>');
+    print_r($students);
+    print('<br>');
+    //Now, we have to give everyone a pencil.
+    //so normally, we would to:
+    $toGive = 'pencil'; //if we have to give another thing later, we can use the same code, just change this variable
+
+    foreach ($students as $studentname => $value){
+        print('--------------------------------------------------------------------------<br><br>');
+        print('Start of foreach loop, which is executed for each element in $students<br>');
+        print('$students is at start of loop iteration<br>');
+        print_r($students);
+        print('<br>');
+        print('Before the code is executed, the key is copied in the variable after the as<br>');
+        print('Which is in this case in this iteration: $studentname : \''.$studentname.'\' <br>');
+        print('and the value is copied in the variable after the =><br>');
+        print('Which is in this case in this iteration: $value : \''.$value.'\' <br>');
+
+
+        //now, we give the student with the current $studentname what we have to give:
+        print('<br><br>Code which is executed in this loop:<br>');
+        print('$students[$studentname]=$toGive;<br>');
+        print('And because $studentname is currently \''.$studentname.'\', this is the same as<br>');
+        print('$students[\''.$studentname.'\']=$toGive;<br>');
+        $students[$studentname]=$toGive;
+        print('<br><br>');
+        print('$students is at end of loop iteration<br>');
+        print_r($students);
+        print('<br>');
+
+        print('--------------------------------------------------------------------------<br><br>');
+    }
+
+    print('After giving everyone pencil<br>');
+    print_r($students);
+    print('<br>');
+
+    print('End Result of Code in Description<br>');
+    unset($students);
+    unset($toGive);
+
+
+    //END DESCRIPTION
 
 
     /*
@@ -119,7 +191,7 @@ $testFunction = function(){
     Now, do the same, but this time give everyone 'textbook'
      */
 
-//end TODO
+//END TODO
     if(!isset($students)){
         $students = array();
     }
@@ -170,7 +242,7 @@ $testFunction = function(){
     And all students (the old ones and Franz) need now an 'exercise book'
      */
 
-//end TODO
+
     if(!isset($students)){
         $students = array();
     }
@@ -182,8 +254,6 @@ $testFunction = function(){
     if(count($students)==0){
         return false;
     }
-
-    $shouldbe = 'exercise book';
     $studentsContent = null;
 
     foreach($students as $key => $value){
@@ -197,17 +267,15 @@ $testFunction = function(){
     }
 
 
-    if($studentsContent== $shouldbe && $students['Franz']==$shouldbe){
+    if($studentsContent== 'textbook'){
         return true;
-    }elseif ($students['Franz']!='exercise book'){
-        print("<br>Error: Hey, you forgot Franz<br>");
     }
-    print("<br>You have made an error, all students should have $shouldbe<br>");
+    print("<br>You have made an error, all students should have 'textbook'<br>");
     return false;
 };
 
 $exerciceSheet->addExercice(new Exercice("
     
-       <h4>Textbook with loop and new student</h4>
+       <h4>Textbook with loop</h4>
     if you made it right, you should not get error message. 
     ",$testFunction));
