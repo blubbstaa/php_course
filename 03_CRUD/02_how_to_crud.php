0d4fca327632dbc8c2e10be39f6b56b3e38c716c
BacLuc <lucius.bachmann@gmx.ch>
2016-10-18 19:45:45 +0100

    added read

--- 03_CRUD/02_how_to_crud.php
+++ 03_CRUD/02_how_to_crud.php
@@ -60,6 +60,8 @@ $testFunction = function(){
     /*
      BEGIN TODO
      Create now the table students with firstname, name, class and then insert 3 rows into it,
+    pay attention that you don't have to create it when it already exists
+    and you don't have to insert data when there is already data inside
     as always with prepared statements.
     Then, you print an html table with it
 
@@ -133,16 +135,7 @@ $testFunction = function(){
                 $errors++;
 
             }
-            $statement = $dblk->prepare("DROP TABLE IF EXISTS students;");
-            //then execute it
 
-            if ( ! $statement->execute())
-            {
-                ob_start();
-                print_r($statement->errorInfo());
-                $errorinfo = ob_get_clean();
-                throw new \Exception($errorinfo);
-            }
             if($errors==0) {
                 return true;
             }
@@ -164,3 +157,183 @@ $exerciceSheet->addExercice(new Exercice("
        <h4>R Read</h4>
     if you made it right, you should see no error messages
     ",$testFunction));
+
+
+
+
+
+
+/*
+ BEGIN DESCRIPTION
+Now, we want to be able to create a new entry via gui.
+to do that, we need a form. so on top of the table, we put a link which passes a variable via
+get. And we make a big if statement around everything we did before. if the get parameter is set,
+we display the form instead. and then we need another if statement
+when the form is submitted.
+first, we need a link before our table:
+//with <a href=''></a> we create a link
+//with $_SERVER['PHP_SELF'], we call the same script again
+//? after the question mark, you can add get parameters to the url.
+// we add the variable viewCR to decide whether we want the form view or not, and set it to form when the link is clicked
+print("<a href='".$_SERVER['PHP_SELF']."?viewCR=form' >Create Entry</a>");
+print("<table>");
+//...code to display table
+
+
+then, we wrap the whole code into an if else statement
+if($_GET['viewCR']=='form'){
+       //we create the form
+        //the form action determines, where the form should be sendt to.
+        //we distinguish the exercises with the viewCR variable
+        print("
+       <form action='".$_SERVER['PHP_SELF']."?viewCR=table' method='POST'>
+            <label for='viewCRfirstname'>Firstname</label>
+            <input type='text' name='firstname' id='viewCRfirstname' />
+            <label for='viewCRname'>Name</label>
+            <input type='text' name='name' id='viewCRname' />
+            <input type='submit' value='submit' name='submit' id='viewCRname'>
+      </form>
+        ");
+}else{
+    print("<a href='".$_SERVER['PHP_SELF']."?viewCR=form' >Create Entry</a>");
+    print("<table>");
+    //..code to display table
+}
+
+
+and then, before we put the handling of the post submission
+
+if(isset($_POST['submit'] && $_GET['viewCR']=='table'){
+    //sanitize a little the input
+    $firstname = filter_var($_POST['firstname'], FILTER_SANITIZE_STRING);
+     $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    //look if all fields are filled
+    if(strlen($firstname)>0 && strlen($name)>0){
+        $insert = $dblk->prepare("INSERT INTO students (firstname, name)VALUES(?,?);
+        $insert->bindParam(1,$firstname);
+        $insert->bindParam(2,$name);
+        if ( ! $statement->execute())
+            {
+                ob_start();
+                print_r($statement->errorInfo());
+                $errorinfo = ob_get_clean();
+                throw new \Exception($errorinfo);
+            }
+          print("Row successfully inserted<br>");
+
+    }else{
+        print("Error, please provide a Firstname and a Name. Row was not inserted<br>");
+
+    }
+}
+
+if($_GET['viewCR']=='form'){
+//.... code we had before
+
+
+
+END DESCRIPTION
+ */
+
+
+$testFunction = function(){
+    ob_start();
+    /*
+     BEGIN TODO
+           Now use the code of the first task (with the class column additionally), and add the functionality to add
+            a Row via the User Interface.
+    */
+
+    // END TODO
+    $printed = ob_get_contents();
+
+    if(!isset($dblk)){
+        $dblk = null;
+    }
+
+    if(is_object($dblk)){
+        if($dblk instanceof PDO) {
+            $statement = $dblk->prepare("SELECT column_name, data_type,column_key, extra 
+                                                                          FROM information_schema.columns 
+                                                                          WHERE TABLE_NAME = 'students'");
+            //then execute it
+
+            if ( ! $statement->execute())
+            {
+                ob_start();
+                print_r($statement->errorInfo());
+                $errorinfo = ob_get_clean();
+                throw new \Exception($errorinfo);
+            }
+
+            //fetch the data
+            $res=$statement->fetchAll(PDO::FETCH_ASSOC);
+            $errors = 0;
+            $solution = array(
+                array(
+                    "column_name" => 'id',
+                    "data_type" => 'int',
+                    "column_key" => 'PRI',
+                    "extra" => 'auto_increment',
+
+                ),
+                array(
+                    "column_name" => 'firstname',
+                    "data_type" => 'text',
+                    "column_key" => '',
+                    "extra" => '',
+
+                ),
+                array(
+                    "column_name" => 'name',
+                    "data_type" => 'text',
+                    "column_key" => '',
+                    "extra" => '',
+
+                ),
+                array(
+                    "column_name" => 'class',
+                    "data_type" => 'text',
+                    "column_key" => '',
+                    "extra" => '',
+
+                ),
+
+
+            );
+            if(count($res)!=count($solution)){
+                print("<br>Error there is no table called students, or it has not the required columns<br>");
+                $errors++;
+            }
+            if(stripos($printed, "table")!==false){
+
+            }else{
+                print("<br>Please print your result as html table<br>");
+                $errors++;
+
+            }
+
+            if($errors==0) {
+                return true;
+            }
+            return false;
+
+
+        }
+    }
+    print("<br>something went wrong<br>");
+
+
+
+
+    return false;
+};
+
+$exerciceSheet->addExercice(new Exercice("
+
+       <h4>CR Create and Read</h4>
+    if you made it right, you should see no error messages
+    ",$testFunction));
+
+
+
