cf0231869a4fdf2003491160b2b44bffbeab4562
BacLuc <lucius.bachmann@gmx.ch>
2016-10-19 11:38:48 +0100

    add exercise crud again

---
+++ 03_CRUD/03_exercise_crud_again.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucius
+ * Date: 19.10.16
+ * Time: 11:37
+ */
+
+/*
+You created the crud now guided.
+Do it now again for a todolist with the fields task and duedate
+
+* */
